b0bef1dae79e2acdba00465b9f07fa621a78373a
mtrade2030 <eliezerz2030@gmail.com>
2025-05-07 10:12:06 +0000

    alteracao1

---
+++ nivelmestrecodar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (){
+
+    char produtoA[30] = "Produto A";
+    char produtoB[30] = "Produto B";
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    printf ("Produto %s tem estoque %u e o valor unitario é %.2f\n", produtoA, estoqueA, valorA);
+    printf ("Produto %s te, estoque %u e o valor unitario é %.2f\n", produtoB, estoqueB, valorB);
+    
+    
+
+  
+    return 0;
+
+
+
+}
